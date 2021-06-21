3c8a93e788e965b73c3711b9e83217dc6ad30b50
CorentinPineault <p-corentin@outlook.fr>
2021-06-21 11:24:40 +0200

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext cxt =
+				new ClassPathXmlApplicationContext("spring-beans.xml");
+		FoodPantry f1 = (FoodPantry) cxt.getBean("bean1");
+		FoodPantry f2 = (FoodPantry) cxt.getBean("bean2");
+		System.out.println(f1.toString());
+		System.out.println(f2.toString());
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/FoodPantry.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+public class FoodPantry {
+    private String address;
+    private int nbCans;
+    private int nbBreads;
+    private String owner;
+    private Float water;
+
+    public FoodPantry(String address, int nbCans, int nbBreads, String owner, Float water) {
+        this.address = address;
+        this.nbCans = nbCans;
+        this.nbBreads = nbBreads;
+        this.owner = owner;
+        this.water = water;
+    }
+
+    @Override
+    public String toString() {
+        return "FoodPantry{" +
+                "address='" + address + '\'' +
+                ", nbCans=" + nbCans +
+                ", nbBreads=" + nbBreads +
+                ", owner='" + owner + '\'' +
+                ", water=" + water +
+                '}';
+    }
+}
